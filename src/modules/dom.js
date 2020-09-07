5ef8a8e418e2db5c21515139e13bb1a39b36e5e9
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-07 15:07:44 -0300

    Add form
    - new mthod to create form in dom.js

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -8,4 +8,23 @@ function creator(parent, newElement, position) {
   return child;
 }
 
+function formSearch(parent) {
+  const form = creator(parent, 'form', 'append');
+
+  const input = creator(form, 'input', 'append');
+  input.setAttribute('name', 'search');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Write the city name here');
+
+  const button = creator(form, 'button', 'append');
+  button.setAttribute('type', 'submit');
+
+  return form;
+}
+
+function createWeatherTable(content) {
+  const main = creator(content, 'main', 'append');
+  const formSearch = creator(content, 'form');
+}
+
 export default creator;

6b6701dcf3a06f035815ff7af6867674c5b636fe
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-06 23:07:56 -0300

    Add config files and file structure
    - Webpack
    - Eslint and stylelint

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/bundle.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};

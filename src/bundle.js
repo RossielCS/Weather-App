1f81e6a8661b7409f684a863b3e5747961fb87ae
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-07 00:05:44 -0300

    Add function to get weather
    - Get city name and return the weather data

--- src/bundle.js
+++ src/bundle.js
@@ -1,1 +1,17 @@
+
 import './assets/stylesheets/style.scss';
+
+function getWeather(cityName) {
+  cityName = cityName.replace(/ /g, '+');
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=7a075fa45323323813d5c357e54b030e`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      document.getElementById('content').innerHTML = `${JSON.stringify(response)}`;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getWeather('Buenos Aires');
